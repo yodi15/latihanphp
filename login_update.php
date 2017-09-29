3b6ac31adfcf499f850eea85697b4835ab911d4a
yodi15 <yodimartinkaah@gmail.com>
2017-09-29 07:26:27 +0700

    LEVEL 2 - Creating Update Records Form
    LEVEL 2 - Creating Update Records Form

---
+++ login_update.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+	<div class="col-mc-6">
+		<form action="login_create.php" method="post">
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" name="username" class="form-control" />
+			</div>
+			<div class="form-group">
+				<label for="username">Password</label>
+				<input type="text" name="password" class="form-control" />
+			</div>
+			<div class="form-group">
+				<select name="id" class="form-control">
+				<option value="1">1</option>
+				</select>
+			</div>
+			<input type="submit" name="submit" value="UPDATE" class="btn btn-primary" />
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
