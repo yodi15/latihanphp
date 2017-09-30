d8482caef08d01f9c19afde2ceb9d582927317bb
yodi15 <yodimartinkaah@gmail.com>
2017-09-30 23:34:14 +0700

    DELETE - DOSEN
    DELETE DATA FOR DOSEN

--- Pertemuan 2/mahasiswa/proses_dosen.php
+++ Pertemuan 2/mahasiswa/proses_dosen.php
@@ -11,6 +11,9 @@ if($_GET['action'] =="add") {
 			  SET kode_dosen = '$_POST[kode_dosen]',
 			      nama_dosen = '$_POST[nama_dosen]'
 			  WHERE id = $_POST[id]";
+}else if ($_GET['action'] == "delete"){
+	$query = "DELETE FROM dosen
+			  WHERE id=$_GET[id]";
 }
 
 mysqli_query($koneksi, $query);
